367b0fd8826b6de70ccd5c7090acfd6b55ec461b
Abhishek Sahani <abhisheksahani653@gmail.com>
2022-03-20 21:30:57 +0530

    Add post signup api for user signup

--- src/pages/authentication-pages/signup/Signup.jsx
+++ src/pages/authentication-pages/signup/Signup.jsx
@@ -1,51 +1,130 @@
+import { useState } from "react";
+import axios from "axios";
+
 function Signup() {
+  const [userSignupData, setUserSignupData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  function handleSignup() {
+    const userData = {
+      email: userSignupData.email,
+      password: userSignupData.password,
+    };
+
+    (async () => {
+      try {
+        const response = await axios.post("/api/auth/signup", userData);
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }
+
   return (
     <section className="login_form_container d-flex">
-      <form
-        className="card-basic login_form"
-        onsubmit="event.preventDefault()"
-      >
+      {console.log(userSignupData)}
+      <div className="card-basic login_form">
         <h2 className="t-align-center mt-2 mb-2">Signup</h2>
+
         <div className="inp-container mb-1">
-          <label
-            className="inp-label d-block inp-label-required login_inp_label_resize"
-            for=""
-          >
+          <label className="inp-label d-block inp-label-required login_inp_label_resize">
+            First name
+          </label>
+          <input
+            className="inp login_inp_resize"
+            id="inp-email"
+            placeholder="Enter your first name"
+            value={userSignupData.firstName}
+            onChange={(e) =>
+              setUserSignupData({
+                ...userSignupData,
+                firstName: e.target.value,
+              })
+            }
+          />
+        </div>
+
+        <div className="inp-container mb-1">
+          <label className="inp-label d-block inp-label-required login_inp_label_resize">
+            Last name
+          </label>
+          <input
+            className="inp login_inp_resize"
+            id="inp-email"
+            placeholder="Enter your last name"
+            value={userSignupData.lastName}
+            onChange={(e) =>
+              setUserSignupData({ ...userSignupData, lastName: e.target.value })
+            }
+          />
+        </div>
+
+        <div className="inp-container mb-1">
+          <label className="inp-label d-block inp-label-required login_inp_label_resize">
             Email address
           </label>
           <input
             className="inp login_inp_resize"
             id="inp-email"
             placeholder="Enter your email address"
+            value={userSignupData.email}
+            onChange={(e) =>
+              setUserSignupData({ ...userSignupData, email: e.target.value })
+            }
           />
 
-          <div className="err-msg-container d-none">
+          {/* <div className="err-msg-container d-none">
             <span>
-              <i className="fa fa-exclamation-circle err-icon"></i>Enter your email
-              address!
+              <i className="fa fa-exclamation-circle err-icon"></i>Enter your
+              email address!
             </span>
-          </div>
+          </div> */}
         </div>
 
         <div className="inp-container mb-1">
-          <label
-            className="inp-label d-block inp-label-required login_inp_label_resize"
-            for=""
-          >
+          <label className="inp-label d-block inp-label-required login_inp_label_resize">
             Password
           </label>
           <input
             className="inp login_inp_resize"
             id="inp-password"
             placeholder="Enter password"
+            value={userSignupData.password}
+            onChange={(e) =>
+              setUserSignupData({ ...userSignupData, password: e.target.value })
+            }
           />
 
-          <div className="err-msg-container d-none">
+          {/* <div className="err-msg-container d-none">
             <span>
               <i className="fa fa-exclamation-circle err-icon"></i>Enter your
               password!
             </span>
-          </div>
+          </div> */}
+        </div>
+
+        <div className="inp-container mb-1">
+          <label className="inp-label d-block inp-label-required login_inp_label_resize">
+            Confirm Password
+          </label>
+          <input
+            className="inp login_inp_resize"
+            id="inp-email"
+            placeholder="Enter your password again"
+            value={userSignupData.confirmPassword}
+            onChange={(e) =>
+              setUserSignupData({
+                ...userSignupData,
+                confirmPassword: e.target.value,
+              })
+            }
+          />
         </div>
 
         <div className="inp-container mb-1">
@@ -56,16 +135,20 @@ function Signup() {
             </label>
           </div>
 
-          <div className="err-msg-container d-none">
+          {/* <div className="err-msg-container d-none">
             <span>
-              <i className="fa fa-exclamation-circle err-icon"></i>Accept our terms
-              & policy before going forward!
+              <i className="fa fa-exclamation-circle err-icon"></i>Accept our
+              terms & policy before going forward!
             </span>
-          </div>
+          </div> */}
         </div>
 
         <div className="inp-container ml-1 mb-1">
-          <button className="btn cta-btn login_custom_btn" type="submit">
+          <button
+            className="btn cta-btn login_custom_btn"
+            type="button"
+            onClick={() => handleSignup()}
+          >
             Signup
           </button>
         </div>
@@ -77,7 +160,7 @@ function Signup() {
             </a>
           </small>
         </div>
-      </form>
+      </div>
     </section>
   );
 }
