e4cea938759a738fa793bdbe34318484d3ca7f39
Abhishek Sahani <abhisheksahani653@gmail.com>
2022-07-10 16:52:17 +0530

    Address page fixes

--- src/pages/address/Address.jsx
+++ src/pages/address/Address.jsx
@@ -1,93 +1,124 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar } from "../../components";
 import "./address.css";
-import { useNavigate, NavLink } from "react-router-dom";
 
 const userProfileLinks = ["Profile", " Addresses", "Orders", "Setting"];
 
 function Address() {
+  const [isDisabled, setIsDisabled] = useState(true);
+
   return (
     <div>
       <Navbar />
 
-      <ul className="categories_navTabs_list mb-2">
-        {userProfileLinks.map((link, index) => {
-          return (
-            <li key={index}>
-              <NavLink
-                to="/profile"
-                className="categories_navLink categories_navLink1"
-              >
-                {link}
-              </NavLink>
-            </li>
-          );
-        })}
-      </ul>
-
       <section className="d-flex justify-cont-center address-form-container">
         <div className="address-form-child-container">
-          <h2 className="address-title">Add a new address</h2>
+          <div className="d-flex justify-content-between align-items-top">
+            <h2 className="address-title">
+              Add address <i class="fa-solid fa-location-dot location-icon"></i>
+            </h2>
+
+            <button
+              className="btn cta-btn btn-small"
+              onClick={() => setIsDisabled(false)}
+            >
+              <i class="fa-solid fa-pencil pencil-icon"></i> Edit
+            </button>
+          </div>
 
           <div className="address-input-container">
             <label className="address-label">Country/Region</label>
-            <input className="address-input" type="text" />
+            <input
+              className="address-input"
+              type="text"
+              disabled={isDisabled}
+            />
           </div>
 
           <div className="address-input-container">
             <label className="address-label">Full name</label>
-            <input className="address-input" type="text" />
+            <input
+              className="address-input"
+              type="text"
+              disabled={isDisabled}
+            />
           </div>
 
           <div className="address-input-container">
             <label className="address-label">Mobile number</label>
-            <input className="address-input" type="text" />
+            <input
+              className="address-input"
+              type="text"
+              disabled={isDisabled}
+            />
           </div>
 
           <div className="address-input-container">
             <label className="address-label">Pincode</label>
-            <input className="address-input" type="text" />
+            <input
+              className="address-input"
+              type="text"
+              disabled={isDisabled}
+            />
           </div>
 
           <div className="address-input-container">
             <label className="address-label">
               Flat, House no., Building, Company, Apartment
             </label>
-            <input className="address-input" type="text" />
+            <input
+              className="address-input"
+              type="text"
+              disabled={isDisabled}
+            />
           </div>
 
           <div className="address-input-container">
             <label className="address-label">
               Area, Street, Sector, Village
             </label>
-            <input className="address-input" type="text" />
+            <input
+              className="address-input"
+              type="text"
+              disabled={isDisabled}
+            />
           </div>
 
           <div className="address-input-container">
             <label className="address-label">Landmark</label>
-            <input className="address-input" type="text" />
+            <input
+              className="address-input"
+              type="text"
+              disabled={isDisabled}
+            />
           </div>
 
           <div className="d-flex gap-3">
             <div className="d-flex f-direction-col">
               <label className="address-label">Town/City</label>
-              <input className="address-input" type="text" />
+              <input
+                className="address-input"
+                type="text"
+                disabled={isDisabled}
+              />
             </div>
 
             <div className="d-flex f-direction-col">
               <label className="address-label">State</label>
-              <input className="address-input" type="text" />
+              <input
+                className="address-input"
+                type="text"
+                disabled={isDisabled}
+              />
             </div>
           </div>
 
-          <div className="d-flex address-input-row-container">
-            <input type="checkbox" />
-            <label className="address-label">
-              Make this my default address
-            </label>
-          </div>
-
-          <button className="btn cta-btn mb-2">Save address</button>
+          <button
+            className="btn cta-btn mb-2"
+            onClick={() => handleSaveAddress}
+          >
+            Save address
+          </button>
         </div>
       </section>
     </div>
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,32 +1,16 @@
 import React from "react";
 import { Navbar } from "../../components/index";
 import "./profile.css";
-import { NavLink } from "react-router-dom";
-
-const userProfileLinks = ["Profile", " Addresses", "Orders", "Setting"];
+import { useNavigate } from "react-router-dom";
 
 function Profile() {
   const username = localStorage.getItem("username");
   const email = localStorage.getItem("email");
+  const navigate = useNavigate();
 
   return (
     <div>
       <Navbar />
-
-      <ul className="categories_navTabs_list mb-2">
-        {userProfileLinks.map((link, index) => {
-          return (
-            <li key={index}>
-              <NavLink
-                to="/profile"
-                className="categories_navLink categories_navLink1"
-              >
-                {link}
-              </NavLink>
-            </li>
-          );
-        })}
-      </ul>
 
       <section className="d-flex justify-cont-center">
         <div className="profile-page-container card-basic width-auto">
@@ -43,9 +27,16 @@ function Profile() {
           <label className="inp-label d-block login_inp_label_resize inherit-clr mb-1">
             Email : {email}
           </label>
-          <label className="inp-label d-block login_inp_label_resize inherit-clr mb-1">
+          <label className="inp-label d-block login_inp_label_resize inherit-clr mb-2">
             Password : *******************
           </label>
+
+          <button
+            className="btn cta-btn btn-small"
+            onClick={() => navigate("/address")}
+          >
+            Add address
+          </button>
         </div>
       </section>
     </div>
