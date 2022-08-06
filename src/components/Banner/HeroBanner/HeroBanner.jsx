0610228252bbfe39fbb1234578188524a283e120
Abhishek Sahani <abhisheksahani653@gmail.com>
2022-08-06 21:06:37 +0530

    Feat Carousel

--- src/components/Banner/HeroBanner/HeroBanner.jsx
+++ src/components/Banner/HeroBanner/HeroBanner.jsx
@@ -1,13 +1,32 @@
 import "./heroBanner.css";
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const bannerImages = ["banner-bg-img1", "banner-bg-img2", "banner-bg-img3"];
 
 function HeroBanner() {
+  const [bannerImg, setBannerImg] = useState(0);
+
   const navigate = useNavigate();
+
+  useEffect(() => {
+    let bannerImgTimer = setTimeout(() => {
+      if (bannerImg == 2) {
+        setBannerImg(0);
+      } else {
+        setBannerImg((prev) => prev + 1);
+      }
+    }, 3000);
+
+    return () => {
+      clearTimeout(bannerImgTimer);
+    };
+  }, [bannerImg]);
 
   return (
     <section
       onClick={() => navigate("/products")}
-      className="homepage_banner_container cursor-p"
+      className={`cursor-p ${bannerImages[bannerImg]} `}
     >
       <div className="banner_content_container"></div>
     </section>
